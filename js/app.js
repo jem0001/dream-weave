fd9453068169ea254d884f57287f6120274247c6
jem0001 <154291007+jem0001@users.noreply.github.com>
2023-12-20 13:18:02 +0800

    Add files via upload

--- js/app.js
+++ js/app.js
@@ -1,54 +1,30 @@
-//step 1: get DOM
-let nextDom = document.getElementById("next");
-let prevDom = document.getElementById("prev");
-
 let carouselDom = document.querySelector(".carousel");
 let SliderDom = carouselDom.querySelector(".carousel .list");
 let thumbnailBorderDom = document.querySelector(".carousel .thumbnail");
-let thumbnailItemsDom = thumbnailBorderDom.querySelectorAll(".item");
-let timeDom = document.querySelector(".carousel .time");
-
-thumbnailBorderDom.appendChild(thumbnailItemsDom[0]);
 let timeRunning = 2200;
-let timeAutoNext = 3000;
+let timeAutoNext = 5000;
 
-nextDom.onclick = function () {
-  showSlider("next");
-};
-
-prevDom.onclick = function () {
-  showSlider("prev");
-};
-let runTimeOut;
-let runNextAuto = setTimeout(() => {
-  next.click();
-}, timeAutoNext);
-function showSlider(type) {
+// Function to show the next slide
+function showNextSlide() {
   let SliderItemsDom = SliderDom.querySelectorAll(".carousel .list .item");
   let thumbnailItemsDom = document.querySelectorAll(
     ".carousel .thumbnail .item"
   );
 
-  if (type === "next") {
-    SliderDom.appendChild(SliderItemsDom[0]);
-    thumbnailBorderDom.appendChild(thumbnailItemsDom[0]);
-    carouselDom.classList.add("next");
-  } else {
-    SliderDom.prepend(SliderItemsDom[SliderItemsDom.length - 1]);
-    thumbnailBorderDom.prepend(thumbnailItemsDom[thumbnailItemsDom.length - 1]);
-    carouselDom.classList.add("prev");
-  }
+  SliderDom.appendChild(SliderItemsDom[0]);
+  thumbnailBorderDom.appendChild(thumbnailItemsDom[0]);
+  carouselDom.classList.add("next");
+
   clearTimeout(runTimeOut);
   runTimeOut = setTimeout(() => {
     carouselDom.classList.remove("next");
-    carouselDom.classList.remove("prev");
   }, timeRunning);
-
-  clearTimeout(runNextAuto);
-  runNextAuto = setTimeout(() => {
-    next.click();
-  }, timeAutoNext);
 }
+
+// Set up auto-next functionality
+let runNextAuto = setInterval(showNextSlide, timeAutoNext);
+
+// Additional code for window.onload if needed
 window.onload = function () {
   window.scrollTo(0, 0);
 };
